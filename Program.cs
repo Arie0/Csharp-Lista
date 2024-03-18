e5b9f8c8331a7a56bfd28bd31908593fed95d7fe
Arie0 <arielykarla3@gmail.com>
2024-03-18 09:16:54 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendendoListas
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            List<string> clientes = new List<string>();
+            clientes.Add("Arie");
+            clientes.Add("Carla");
+            string pessoa = "Amelia";
+            clientes.Add(pessoa);
+
+            foreach (string cliente in clientes)
+            {
+                Console.WriteLine(cliente);
+            }
+
+            clientes.RemoveAt(1);
+
+            Console.WriteLine("*************");
+
+            foreach (string cliente in clientes)
+            {
+                Console.WriteLine(cliente);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+}
